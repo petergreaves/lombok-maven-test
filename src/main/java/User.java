1dfafd407302164abd95ef7ed3b13bf7db808f9b
peter greaves <peter.greaves64@gmail.com>
2021-08-03 14:57:27 +0100

    successful tests

---
+++ src/main/java/User.java
@@ -0,0 +1,15 @@
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+}
